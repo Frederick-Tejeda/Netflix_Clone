dfe97c20c11734d8ccc85b277318e9d7f195c54b
Frederick-Tejeda <103900166+Frederick-Tejeda@users.noreply.github.com>
2024-04-17 09:06:59 -0400

    Close to add Astro studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  integrations: [db()]
+});
