325bf2e23ee40d1c89fc5b4952b0aadca23c00bb
Frederick-Tejeda <103900166+Frederick-Tejeda@users.noreply.github.com>
2024-04-18 11:41:01 -0400

    Modal and DB added

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,19 @@
-import { defineDb } from 'astro:db';
+import { defineDb, column, defineTable } from 'astro:db';
+
+// watching: column.text(),
+// watchList: column.text(),
+// favorites: column.text(),
+
+const Users = defineTable({
+  columns: {
+    id: column.number({unique: true}),
+    user: column.text({unique: true}),
+    name: column.text(),
+    keepLoggedIn: column.boolean()
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Users }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,19 @@
-import { db } from 'astro:db';
+import { db, Users } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	// await db.insert(Users).values([
+	// 	{
+	// 		id: Date.now(),
+	// 		user: "ffrederf3@gmail.com",
+	// 		name: "Frederick Tejeda",
+	// 		keepLoggedIn: true
+	// 	},
+	// 	{
+	// 		id: Date.now(),
+	// 		user: "Frederf918@gmail.com",
+	// 		name: "Moises Rodriguez",
+	// 		keepLoggedIn: false
+	// 	}
+	// ])
 }
