d2dc883f4264e98c351d04cc0d04cc30e97c9b2a
Frederick-Tejeda <103900166+Frederick-Tejeda@users.noreply.github.com>
2024-04-19 08:40:23 -0400

    Deployed to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 
-import db from "@astrojs/db";
-
 // https://astro.build/config
+
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [db()]
+  integrations: []
 });
